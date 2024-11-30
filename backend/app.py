620f6a773b5cedd0e578a46fcd075192d20d292a
Glenelly Karla <glenellykarlatbm@gmail.com>
2024-11-29 22:16:53 -0300

    configurando conexão com o neon

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Usar a variável de ambiente para configurar a URL do banco de dados
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("postgresql://neondb_owner:******@ep-dark-king-a54fcoql-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("postgresql://neondb_owner:mK5ErcHOtuv2@ep-dark-king-a54fcoql.us-east-2.aws.neon.tech/neondb?sslmode=require")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
