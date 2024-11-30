1052c2b50c9896564b42195c00309b161fbba0af
Glenelly Karla <glenellykarlatbm@gmail.com>
2024-11-29 21:54:02 -0300

    Reconfigurando

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,26 @@
 from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate  # Importação do Flask-Migrate
 import os
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///userdata.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-frontend_folder = os.path.join(os.getcwd(),"..","frontend")
-dist_folder = os.path.join(frontend_folder,"dist")
+# Configuração do Flask-Migrate
+migrate = Migrate(app, db)  # Inicialização do Migrate com a aplicação e o banco de dados
 
-@app.route("/",defaults={"filename":""})
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend", "dist")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+@app.route("/", defaults={"filename": ""})
 @app.route("/<path:filename>")
 def index(filename):
     if not filename:
         filename = "index.html"
-    return send_from_directory(dist_folder,filename)
-
-
-import routes
-
-with app.app_context():
-    db.create_all()
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+    return send_from_directory(dist_folder, filename)
